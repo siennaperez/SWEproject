283f20fcc7d8746ec54380a1dd28c9f49fda03b7
heirynhr <148991328+heirynhr@users.noreply.github.com>
2025-03-07 01:26:37 -0500

    ProfilePage Edits +Starting Homescreen
    Formatted parts of profile page and started homescreen, need to know how to structure it like where in the folder to put it and link it within the app to see it

--- BeFit/constants/Colors.ts
+++ BeFit/constants/Colors.ts
@@ -8,11 +8,11 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: '#ECEDEE',
+    background: '#151718',
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
     tabIconSelected: tintColorLight,
   },
   dark: {
