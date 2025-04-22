a73a993fbfde27ad69453202f040c5bbabadaeef
jonathandeng7 <jmdeng05@gmail.com>
2025-04-22 17:29:59 -0400

    create post button changed to floating

--- BeFit/app/(login)/ipConfig.js
+++ BeFit/app/(login)/ipConfig.js
@@ -1,4 +1,4 @@
-const ipAddress = 'http://10.136.226.222:3000';  // default IP address
+const ipAddress = 'http://10.138.73.10:3000';  // default IP address
 
 const getIpAddress = () => ipAddress;
 
